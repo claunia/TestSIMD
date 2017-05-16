2639796a3b61a6519cc15d855b7433a78cc1bbd8
Natalia Portillo <claunia@claunia.com>
2017-05-16 13:48:39 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,47 @@
+﻿This is a test reimplementing MD5 using pure managed C# code, with uint, System.Numerics.Vectors.Vector<uint> and Mono.Simd.Vector4ui.
+
+System.Numerics.Vector.IsHardwareAccelerated = false.
+BenchmarkDotNet=v0.10.6, OS=Mac OS X 10.11
+Processor=Intel Core2 Duo CPU P8700 2.53GHz, ProcessorCount=2
+Frequency=10000000 Hz, Resolution=100.0000 ns, Timer=UNKNOWN
+  [Host]     : Mono 5.0.0.100 (2017-02/9667aa6 Fri), 32bit
+  DefaultJob : Mono 5.0.0.100 (2017-02/9667aa6 Fri), 32bit
+
+
+        Method |      Mean |     Error |    StdDev | Relative |
+-------------- |----------:|----------:|----------:|---------:|
+           Md5 |  1.124 ms | 0.0399 ms | 0.1158 ms |  100.00% |
+    Md5Managed |  1.978 ms | 0.0385 ms | 0.0360 ms |  175.97% |
+ Md5Vectorized | 67.204 ms | 1.1802 ms | 1.0462 ms | 5979.00% |
+       Md5Mono |  8.377 ms | 0.1606 ms | 0.1502 ms |  745.28% |
+
+
+System.Numerics.Vector.IsHardwareAccelerated = true.
+BenchmarkDotNet=v0.10.6, OS=Windows 10.0.16193
+Processor=AMD Phenom(tm) 9650 Quad-Core Processor, ProcessorCount=4
+Frequency=2246249 Hz, Resolution=445.1866 ns, Timer=TSC
+  [Host]     : Clr 4.0.30319.42000, 64bit RyuJIT-v4.7.2505.0
+  DefaultJob : Clr 4.0.30319.42000, 64bit RyuJIT-v4.7.2505.0
+
+
+        Method |       Mean |     Error |    StdDev | Relative |
+-------------- |-----------:|----------:|----------:|---------:|
+           Md5 |   212.0 us | 0.9392 us | 0.8785 us |  100.00% |
+    Md5Managed |   734.2 us | 1.9787 us | 1.6523 us |  346.32% |
+ Md5Vectorized | 2,817.4 us | 3.9387 us | 3.2890 us | 1328.96% |
+
+
+System.Numerics.Vector.IsHardwareAccelerated = false.
+BenchmarkDotNet=v0.10.6, OS=Linux
+Processor=Intel Xeon CPU E5-2690 v3 2.60GHz, ProcessorCount=24
+Frequency=10000000 Hz, Resolution=100.0000 ns, Timer=UNKNOWN
+  [Host]     : Mono 5.0.0 (Stable 5.0.0.100/9667aa6), 64bit
+  DefaultJob : Mono 5.0.0 (Stable 5.0.0.100/9667aa6), 64bit
+
+
+        Method |        Mean |      Error |     StdDev | Relative |
+-------------- |------------:|-----------:|-----------:|---------:|
+           Md5 |    272.5 us |   1.733 us |   1.253 us |  100.00% |
+    Md5Managed |    750.7 us |   8.095 us |   7.572 us |  275.49% |
+ Md5Vectorized | 20,467.7 us | 181.869 us | 161.222 us | 7511.08% |
+       Md5Mono |  2,628.7 us |  30.470 us |  27.011 us |  964.66% |
---
+++ TestSIMD/Program.cs
@@ -0,0 +1,81 @@
+﻿//
+//  Program.cs
+//
+//  Author:
+//       Natalia Portillo <claunia@claunia.com>
+//
+//  Copyright (c) 2017 © Claunia.com
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
+using System;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Numerics;
+
+namespace TestSIMD
+{
+	public class MD5Benchs
+	{
+		private const int N = 65536;
+		private readonly byte[] data;
+
+		private readonly MD5 md5 = MD5.Create();
+        private readonly MD5Managed md5m = new MD5Managed();
+		private readonly VectorMD5 md5v =new VectorMD5();
+		private readonly MonoMD5 md5s = new MonoMD5();
+
+		public MD5Benchs()
+		{
+			data = new byte[N];
+			new Random(42).NextBytes(data);
+		}
+
+		[Benchmark]
+		public byte[] Md5() => md5.ComputeHash(data);
+
+        [Benchmark]
+		public byte[] Md5Managed() => md5m.ComputeHash(data);
+
+		[Benchmark]
+		public byte[] Md5Vectorized() => md5v.ComputeHash(data);
+
+        [Benchmark]
+        public byte[] Md5Mono() => md5s.ComputeHash(data);
+	}
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			Console.WriteLine("Vector.IsHardwareAccelerated = {0}", Vector.IsHardwareAccelerated);
+
+			MD5Benchs foo = new MD5Benchs();
+
+			byte[] std = foo.Md5();
+			byte[] mgd = foo.Md5Managed();
+			byte[] vec = foo.Md5Vectorized();
+			byte[] mno = foo.Md5Mono();
+
+			Console.WriteLine("std: {0:x2}{1:x2}{2:x2}{3:x2}", std[0], std[1], std[2], std[3]);
+			Console.WriteLine("mgd: {0:x2}{1:x2}{2:x2}{3:x2}", mgd[0], mgd[1], mgd[2], mgd[3]);
+			Console.WriteLine("vec: {0:x2}{1:x2}{2:x2}{3:x2}", vec[0], vec[1], vec[2], vec[3]);
+			Console.WriteLine("mno: {0:x2}{1:x2}{2:x2}{3:x2}", mno[0], mno[1], mno[2], mno[3]);
+			
+            var summary = BenchmarkRunner.Run<MD5Benchs>();
+		}
+    }
+}
